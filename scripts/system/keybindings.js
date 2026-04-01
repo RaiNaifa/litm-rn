2d92b5d00392b51d92115137c60760278537847e
a.brukouskaya <a.brukouskaya@andersenlab.com>
2026-04-01 20:30:53 +0300

    Updated keybindings

--- scripts/system/keybindings.js
+++ scripts/system/keybindings.js
@@ -2,7 +2,7 @@ import { localize as t } from "../utils.js";
 
 export class KeyBindings {
 	static register() {
-		game.keybindings.register("litm", "openDiceRoller", {
+		game.keybindings.register("litm-rn", "openDiceRoller", {
 			name: t("Litm.ui.dice-roller"),
 			hint: t("Litm.ui.dice-roller-hint"),
 			editable: [
@@ -11,12 +11,46 @@ export class KeyBindings {
 				},
 			],
 			onDown: () => {
-				const sheet = game.user.character?.sheet;
-				if (!sheet)
+				const token = canvas.tokens.controlled[0];
+				const actor = token?.actor;
+
+				if (!actor?.sheet) {
+					const sheet = game.user.character?.sheet;
+	
+					if (sheet) {
+						return sheet.renderRollDialog({ toggle: true });
+					}
+
 					return ui.notifications.warn("Litm.ui.warn-no-character", {
 						localize: true,
 					});
-				return sheet.renderRollDialog({ toggle: true });
+				}
+
+				if (game.user.isGM || actor.isOwner) {
+					return actor.sheet.renderRollDialog({ toggle: true });
+				}
+
+				return ui.notifications.warn("Litm.ui.warn-no-character", {
+					localize: true,
+				});
+			},
+			onUp: () => {},
+			restricted: false,
+			precedence: CONST.KEYBINDING_PRECEDENCE.PRIORITY,
+		});
+
+		game.keybindings.register("litm-rn", "openStoryTagApp", {
+			name: t("Litm.ui.manage-tags"),
+			hint: t("Litm.ui.manage-tags-hint"),
+			editable: [
+				{
+					key: "KeyT",
+				},
+			],
+			onDown: () => {
+				const app = game.litm.storyTags;
+				if (app.rendered) return app.close();
+				return app.render(true);
 			},
 			onUp: () => {},
 			restricted: false,
